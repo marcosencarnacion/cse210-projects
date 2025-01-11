4ea64a9cb981d7670707157b00be81c13b3f3049
marcosencarnacion <enc24001@byui.edu>
2025-01-11 14:11:43 -0500

    while loop added to prompt the user until gets it right.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,29 +6,39 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
+        //# Core Requirements
+
         //#1. Start by asking the user for the magic number and for a guess.
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int guessNumber = int.Parse(Console.ReadLine());
 
-        //# 2. Adding an if statement to prompt the user to guess a higher, lower number or if they guessed it.
-        if (guessNumber < magicNumber)
+        // Initialize the guess variable to ensure it enters the loop
+        int guessNumber = -1;
+
+        //2. Use a while loop to keep asking the user for their guess until they get it right.
+        while (guessNumber != magicNumber)
         {
-            Console.Write("Higher");
-        }
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
 
-        else if (guessNumber > magicNumber)
-        {
-            Console.Write("Lower");
-        }
 
-        else
-        {
-            Console.Write("You guessed it!");
-        }
+            //# 3. Adding an if statement to prompt the user to guess a higher, lower number or if they guessed it.
+            if (guessNumber < magicNumber)
+            {
+                Console.Write("Higher");
+            }
 
+            else if (guessNumber > magicNumber)
+            {
+                Console.Write("Lower");
+            }
+
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
 
     }
 }
