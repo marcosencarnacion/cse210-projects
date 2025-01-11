41ea0a1861451245c195cd49099cd1b5b0fb6f9d
marcosencarnacion <enc24001@byui.edu>
2025-01-11 15:13:24 -0500

    created a do-while loop for user to enter numbers until enter 0

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -11,5 +11,19 @@ class Program
 
         int userInput;
 
+        // Ask the user to enter a series of numbers until they enter 0
+        Console.WriteLine("Enter a list of numbers, type 0 whe finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0) //Only add numbers to the list if it's not 0
+            {
+                numbers.Add(userInput);
+            }
+        }
+
     }
 }
