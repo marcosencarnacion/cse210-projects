103df96f9a313419917507c1ab347c7f0af7f025
marcosencarnacion <enc24001@byui.edu>
2025-01-11 11:22:26 -0500

    Added code to ask user first and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // Ask the user for their name.
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
