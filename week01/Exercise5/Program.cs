85efa1ac13c2c1ae344b9cbddf3d8948276ac31b
marcosencarnacion <enc24001@byui.edu>
2025-01-11 17:02:02 -0500

    defined first function with welcome message

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+
+        //Function #1. DisplayWelcome - Displays the message, "Welcome to the Program!"
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        
+
     }
 }
