e504f3832d51c5c0451c1118ad6ae47dd64bd9f6
marcosencarnacion <enc24001@byui.edu>
2025-01-11 15:10:01 -0500

    Initialize a list to store the numbers

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        //Initialize a list to store the numbers.
+        List<int> numbers = new List<int>();
+
+        int userInput;
+
     }
 }
