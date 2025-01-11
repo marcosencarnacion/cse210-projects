241477ae7cc420254e2a7eaf10658f0f42b97f1c
marcosencarnacion <enc24001@byui.edu>
2025-01-11 13:52:42 -0500

    ask user for magic and guess number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //#1. Start by asking the user for the magic number and for a guess.
+        Console.WriteLine("What is the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What is your guess?");
+        int guessNumber = int.Parse(Console.ReadLine());
+
     }
 }
