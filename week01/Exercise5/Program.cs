efcc95c48666f2b4ef1a36812559cba6bca35ada
marcosencarnacion <enc24001@byui.edu>
2025-01-11 17:20:08 -0500

    calling functions in the main function

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,9 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        // Call DisplayWelcome function
         DisplayWelcome();
 
+        // Call PromptUserName function and store the returned value
+        string name = PromptUserName();
 
+        // Call PromptUserNumber function and store the reunted value
+        int favoriteNumber = PromptUserNumber();
+
+        // Call SquareNumber function and store the squared result
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        // Call DisplayResult function to display the final output
+        DisplayResult(name, squaredNumber);
+
+
+        // Simple Functions 
         //Function #1. DisplayWelcome - Displays the message, "Welcome to the Program!"
         static void DisplayWelcome()
         {
@@ -19,17 +34,28 @@ class Program
         {
             Console.Write("Please enter your name: ");
             string userName = Console.ReadLine();
-            return userName; // Return the name
+            return userName; // Return the entered name
         }
 
-         //Function #3. PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+        //Function #3. PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
         static int PromptUserNumber()
         {
             Console.Write("Please enter your favorite number: ");
-            int userNumber = int.Parse(Console.ReadLine());
+            int userNumber = int.Parse(Console.ReadLine()); // Parse input to integer
             return userNumber;
         }
+
+        //Function #4. SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+        static int SquareNumber(int number)
+        {
+            return number * number; //retunr the square of the number
+        }
         
+        //Function #5. DisplayResult - Accepts the user's name and the squared number and displays them.
+        static void DisplayResult(string userName, int SquaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {SquaredNumber}");
+        }
 
     }
 }
