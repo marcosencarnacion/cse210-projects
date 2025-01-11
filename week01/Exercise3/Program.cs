0ac71f660a904d2921b827e2de2b8da66664c1ea
marcosencarnacion <enc24001@byui.edu>
2025-01-11 13:54:22 -0500

    removed extra line from the Console.Write

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,10 +7,10 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
         //#1. Start by asking the user for the magic number and for a guess.
-        Console.WriteLine("What is the magic number?");
+        Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("What is your guess?");
+        Console.Write("What is your guess? ");
         int guessNumber = int.Parse(Console.ReadLine());
 
     }
