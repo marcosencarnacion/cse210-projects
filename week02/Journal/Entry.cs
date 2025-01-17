2ad6aa3435215d82a242ba3b89584af56f943d3c
marcosencarnacion <enc24001@byui.edu>
2025-01-17 17:18:37 -0500

    added prompts to the list of prompts

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Entry
 {
     public Entry(string date, string promptText, string entryText)
@@ -10,11 +12,11 @@ public class Entry
     public string Date { get; } // Holds the date of the entry, the data cannot be modified after initialization.
     public string PromptText { get; } // Stores the prompt question related with the journal entry.
     public string EntryText { get; } // Stores the user's written response to the prompt.
-    
+
 
     public void Display()
     {
-        
+
     }
 
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
     // List of prompts for the user to choose from
     public List<string> _prompts = new List<string>
     {
-
+        "What made you smile today?",
+        "What is something new you learned today?",
+        "Describe a challenge you overcame today.",
+        "What are you grateful for today?",
+        "What was your favorite moment of the day?"
     };
 
     // Method to get a random prompt from the list.
