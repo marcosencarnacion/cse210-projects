34881f1c4e7af0cea04f551c15942261b601cee0
marcosencarnacion <enc24001@byui.edu>
2025-01-17 00:41:23 -0500

    added entry function Display()

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -3,5 +3,11 @@ public class Entry
     public string Date { get; } // Holds the date of the entry, the data cannot be modified after initialization.
     public string PromptText { get; } // Stores the prompt question related with the journal entry.
     public string EntryText { get; } // Stores the user's written response to the prompt.
+    
+
+    public void Display()
+    {
+        
+    }
 
 }
