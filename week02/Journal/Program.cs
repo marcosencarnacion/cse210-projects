49754d0077082c1e5a40447de7d0c3923dfacfb1
marcosencarnacion <enc24001@byui.edu>
2025-01-17 17:35:32 -0500

    added a while loop for the menu display which will continue until the user quits program.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -8,5 +8,27 @@ class Program
     {
         Journal journal = new(); // Create a new journal object
         PromptGenerator promptGenerator = new(); // Createa new prompt generator object
+
+        DisplayWelcomeMessage(); // Display the welcome message when the program starts
+
+        bool continueRunning = true; // Flag to control the menu loop.
+
+        while (continueRunning)
+        {
+            DisplayMenu(); // Show the menu with options for the user
+            int option;
+
+            // Try to parse the user's input ot an integer
+            if (int.TryParse(Console.ReadLine(), out option))
+            {
+                continueRunning = HandleMenuOption(option, journal, promptGenerator); // Handle the selected option
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please choose a valid option");
+            }
+        }
+
+        Console.WriteLine("Goodbye!"); // Display a goodbye essage when quitting
     }
 }
