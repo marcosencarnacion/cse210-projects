1b49f78231ec3770c7401221d8616335d7ab259b
marcosencarnacion <enc24001@byui.edu>
2025-01-17 01:03:49 -0500

    added attribute and method to the PromptGenerator class

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,11 +3,7 @@ public class PromptGenerator
     // List of prompts for the user to choose from
     public List<string> _prompts = new List<string>
     {
-        "What made you smile today? ",
-        "What is something new you learned today? ",
-        "Describe a challenge you overcame today? ",
-        "What are you grateful for today? ",
-        "What was your favorite moment of the day? "
+
     };
 
     // Method to get a random prompt from the list.
