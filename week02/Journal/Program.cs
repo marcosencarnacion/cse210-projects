5e73e420d32be8539b48349869776c1aa7952435
marcosencarnacion <enc24001@byui.edu>
2025-01-17 17:27:07 -0500

    expression 'new' was simplified for both objects

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Journal journal = new Journal(); // Create a new journal object
-        PromptGenerator promptGenerator = new PromptGenerator(); // Createa new prompt generator object
+        Journal journal = new(); // Create a new journal object
+        PromptGenerator promptGenerator = new(); // Createa new prompt generator object
     }
 }
