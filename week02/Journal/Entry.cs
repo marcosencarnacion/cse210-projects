db3896bf0149b69507edb8d217a11ec8cf0ef8e9
marcosencarnacion <enc24001@byui.edu>
2025-01-17 18:35:24 -0500

    comments added.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -2,6 +2,12 @@ using System;
 
 public class Entry
 {
+    public string Date { get; } // Holds the date of the entry, the data cannot be modified after initialization.
+    public string PromptText { get; } // Stores the prompt question related with the journal entry.
+    public string EntryText { get; } // Stores the user's written response to the prompt.
+
+    
+    // Construct to create a new entry with the date, promtp, and entry text
     public Entry(string date, string promptText, string entryText)
     {
         Date = date;
@@ -9,11 +15,8 @@ public class Entry
         EntryText = entryText;
     }
 
-    public string Date { get; } // Holds the date of the entry, the data cannot be modified after initialization.
-    public string PromptText { get; } // Stores the prompt question related with the journal entry.
-    public string EntryText { get; } // Stores the user's written response to the prompt.
 
-
+    // Method to display the journal
     public void Display()
     {
 
