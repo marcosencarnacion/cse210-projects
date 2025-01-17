158b06bf18ec56a40c233bdbb5af3d914066d39b
marcosencarnacion <enc24001@byui.edu>
2025-01-17 00:50:54 -0500

    added attributes and functions to the Journal class

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,4 +1,24 @@
 public class Journal
 {
+    private List<Entry> _entries; // This is a private list to store all journal entries
 
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string filename)
+    {
+
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        
+    }
 }
