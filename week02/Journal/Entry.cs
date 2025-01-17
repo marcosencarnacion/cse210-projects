3bfd3e9222ab5186506cb0c65d9f9b1e47676296
marcosencarnacion <enc24001@byui.edu>
2025-01-17 00:38:53 -0500

    entry class attributes added

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,4 +1,7 @@
 public class Entry
 {
+    public string Date { get; } // Holds the date of the entry, the data cannot be modified after initialization.
+    public string PromptText { get; } // Stores the prompt question related with the journal entry.
+    public string EntryText { get; } // Stores the user's written response to the prompt.
 
 }
