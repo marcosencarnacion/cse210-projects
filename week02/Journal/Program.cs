27b842fffe650e42b08d96a6f78adb10a3292753
marcosencarnacion <enc24001@byui.edu>
2025-01-17 17:26:23 -0500

    Journal and Prompt generator objects created

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal(); // Create a new journal object
+        PromptGenerator promptGenerator = new PromptGenerator(); // Createa new prompt generator object
     }
 }
