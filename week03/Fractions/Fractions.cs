5078f1e9b50a88bfe2e2029f5fcab874884fe348
marcosencarnacion <enc24001@byui.edu>
2025-01-20 20:53:48 -0500

    Constructors with their parameters are set up

--- week03/Fractions/Fractions.cs
+++ week03/Fractions/Fractions.cs
@@ -6,4 +6,23 @@ public class Fraction
     private int _bottom;
 
 
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+
 }
