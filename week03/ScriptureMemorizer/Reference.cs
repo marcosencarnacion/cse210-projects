0150aaf3b2a4e374bf497dd29392a5a870c814d7
marcosencarnacion <enc24001@byui.edu>
2025-01-24 19:31:26 -0500

    fixed the reference attribute to accept "null" as a value when declaring it in the method.

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -5,7 +5,7 @@ public class Reference
     private string _book;           // Book of the bible, for example, "Matthew"
     private int _chapter;           // Chapter of the book
     private int _verse;             // Starting verse
-    private int _endVerse;          // Ending verse (for ranges of verses)
+    private int? _endVerse;          // Ending verse (for ranges of verses)
 
 
     // Two constructors are defined. One that accepts a book, chapter, and a starting verse.
