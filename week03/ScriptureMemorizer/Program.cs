9fa867af3c304a68048407421dac8a6f994fe28e
marcosencarnacion <enc24001@byui.edu>
2025-01-24 21:44:31 -0500

    Added 3 more scriptures with multi verses in the library.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -13,20 +13,20 @@ class Program
             new Scripture(new Reference("Isaiah", 40, 31), "But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint."),
 
             // New Testament
-            new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."),
+            new Scripture(new Reference("John", 3, 16, 17), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life. For God sent not his Son into the world to condemn the world; but that the world through him might be saved."),
             new Scripture(new Reference("Matthew", 5, 14), "Ye are the light of the world. A city that is set on a hill cannot be hid."),
 
             // Book of Mormon
             new Scripture(new Reference("2 Nephi", 2, 25), "Adam fell that men might be; and men are, that they might have joy."),
-            new Scripture(new Reference("Alma", 37, 6), "Now ye may suppose that this is foolishness in me; but behold I say unto you, that by small and simple things are great things brought to pass; and small means in many instances doth confound the wise."),
+            new Scripture(new Reference("Alma", 37, 6, 7), "Now ye may suppose that this is foolishness in me; but behold I say unto you, that by small and simple things are great things brought to pass; and small means in many instances doth confound the wise. And the Lord God doth work by means to bring about his great and eternal purposes; and by very small means the Lord doth confound the wise and bringeth about the salvation of many souls."),
 
             // Doctrine and Covenants
             new Scripture(new Reference("D&C", 58, 26, 27), "For behold, it is not meet that I should command in all things; for he that is compelled in all things, the same is a slothful and not a wise servant; wherefore he receiveth no reward. Verily I say, men should be anxiously engaged in a good cause, and do many things of their own free will, and bring to pass much righteousness;"),
             new Scripture(new Reference("D&C", 121, 45), "Let thy bowels be full of charity towards all men, and to the household of faith, and let virtue garnish thy thoughts unceasingly; then shall thy confidence wax strong in the presence of God; and the doctrine of the priesthood shall distil upon thy soul as the dews from heaven."),
             
             // Pearl of Great Price
-            new Scripture(new Reference("Moses", 1, 39), "For behold, this is my work and my glory—to bring to pass the immortality and eternal life of man."),
-            new Scripture(new Reference("Abraham", 3, 22), "Now the Lord had shown unto me, Abraham, the intelligences that were organized before the world was; and among all these there were many of the noble and great ones; and God saw these souls that they were good, and he stood in the midst of them, and he said: These I will make my rulers."),
+            new Scripture(new Reference("Moses", 1, 38, 39), "And as one earth shall pass away, and the heavens thereof even so shall another come; and there is no end to my works, neither to my words. For behold, this is my work and my glory—to bring to pass the immortality and eternal life of man."),
+            new Scripture(new Reference("Abraham", 3, 22), "Now the Lord had shown unto me, Abraham, the intelligences that were organized before the world was; and among all these there were many of the noble and great ones;"),
         };
 
         // Create a Random object to pick a random scripture from the library.
