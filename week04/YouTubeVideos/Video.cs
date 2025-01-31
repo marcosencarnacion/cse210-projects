1ded345499f84fb2dcfb3c0007ce3fa0ce6fd830
marcosencarnacion <enc24001@byui.edu>
2025-01-31 16:40:55 -0500

    Comment class created. Attributes added

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,6 +1,14 @@
 using System;
 using System.Collections.Generic;
 
+
+public class Comment
+{
+    private string _commentName;
+    private string _commentText;
+}
+
+
 public class Video
 {
     private string _title;
@@ -8,3 +16,4 @@ public class Video
     private int _length; // in seconds
     private List<Comment> _comments;
 }
+
