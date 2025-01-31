0aba2b154e0329f2293c2d636988985bfc911196
marcosencarnacion <enc24001@byui.edu>
2025-01-31 18:57:54 -0500

    deleted icons

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -11,7 +11,7 @@ public class Program
         // Create first video and add comments.
         Video video1 = new Video("C# Basics Tutorial", "Brais Moure", 600);
         video1.AddComment(new Comment("Carlos", "I was thinking about switching technologies, but thanks to this video, I believe I will specialize in ML with C#. Thank you, Mauro, you are amazing!"));
-        video1.AddComment(new Comment("Angela", "I love you Brais, everything I look for, you've made a course on! Thank you so much for so much quality content 😍😍"));
+        video1.AddComment(new Comment("Angela", "I love you Brais, everything I look for, you've made a course on! Thank you so much for so much quality content."));
         video1.AddComment(new Comment("Jorge", "Another remarkable course... thank you very much."));
         video1.AddComment(new Comment("Israel", "Very interesting, I got a bit confused with the topic of constructors, but I understood it in the end. Thank you."));
         videos.Add(video1);
