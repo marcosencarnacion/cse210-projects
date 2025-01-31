c3cf8949d20fceb536dc79d2199d09e611e58a9b
marcosencarnacion <enc24001@byui.edu>
2025-01-31 18:45:25 -0500

    added video 3 and 4 to the list

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -19,24 +19,38 @@ public class Program
         // Create second video and add comments.
         Video video2 = new Video("Object-Oriented Programming, Simplified", "Mosh Hamedani", 453);
         video2.AddComment(new Comment("Erik", "Hey, if you you watched the video in double speed, you master oop in even three and a half minutes!"));
-        video2.AddComment(new Comment("Sumani", "7 minutes and alot of things in detail. Thanks Mosh."));
+        video2.AddComment(new Comment("Sumani", "7 minutes and a lot of things in detail. Thanks Mosh."));
         video2.AddComment(new Comment("Geeta", "Hi Mosh. Really your videos are smooth understandable. Thanks"));
         video2.AddComment(new Comment("Stephanie", "You helped me understand OOP in minutes more than my professors did in hours of lecture. Thank you!!"));
         videos.Add(video2);
 
         // Create third video and add comments.
-        Video video3 = new Video("C# classes 📦", "Bro Code", 300);
+        Video video3 = new Video("C# classes", "Bro Code", 300);
         video3.AddComment(new Comment("Zoe", "You are a legend, I'm studying it and my teachers explain it as if im already experienced. This is a huge help"));
         video3.AddComment(new Comment("Regie", "Very good clear and succinct presentation. Thanks."));
-        video3.AddComment(new Comment("Alex", "Good job you are always explaining very clearly 👏"));
-        video3.AddComment(new Comment("Sreenivas", "Awesome explanation ❤ Thank you"));
+        video3.AddComment(new Comment("Alex", "Good job you are always explaining very clearly."));
+        video3.AddComment(new Comment("Sreenivas", "Awesome explanation. Thank you"));
+        videos.Add(video3);
 
         // Create fourth video and add comments.
         Video video4 = new Video("Introduction to Classes and Objects", "YK Sugi", 1200);
         video4.AddComment(new Comment("Jonah", "The constructor explanation is gold!"));
         video4.AddComment(new Comment("Clane", "My whole programming for the semester taught powerfully in a very small amount of time in under 20mins. Deserve more subs."));
         video4.AddComment(new Comment("Vibhanshu", "Wow 2021 here still the best video on you tube for classes and object."));
-        video4.AddComment(new Comment("Tracer", "I lost my interest in coding 3 years ago & because of you I am getting into this again. ❤️"));
+        video4.AddComment(new Comment("Tracer", "I lost my interest in coding 3 years ago & because of you I am getting into this again."));
+        videos.Add(video4);
+
+        // Iterate through the list of videos and display their information.
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length (seconds): {video.Length}");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+            Console.WriteLine("Comments:");
+            Console.WriteLine(video.DisplayAllComments());
+        }
 
     }
 }
