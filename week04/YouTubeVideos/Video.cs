bc05736325226277dd0afdf4b8dada8dcbbd24ee
marcosencarnacion <enc24001@byui.edu>
2025-01-31 16:37:31 -0500

    Video class created, and attributes added

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length; // in seconds
+    private List<Comment> _comments;
+}
