eff8d2458d516ab2e9f46577cd0f9527fa738e07
marcosencarnacion <enc24001@byui.edu>
2025-01-31 16:54:29 -0500

    added constructor in the Video class

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -4,8 +4,15 @@ using System.Collections.Generic;
 
 public class Comment
 {
-    private string _commentName;
+    private string _commenterName;
     private string _commentText;
+
+    // Constructor
+    public Comment(string commenterName, string commentText)
+    {
+        _commenterName = commenterName;
+        _commentText = commentText;
+    }
 }
 
 
@@ -15,5 +22,14 @@ public class Video
     private string _author;
     private int _length; // in seconds
     private List<Comment> _comments;
+
+    // Constructor that initializes the attributes and creates an empty list for comments.
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>();
+    }
 }
 
