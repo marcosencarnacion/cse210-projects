3b5d1044f9d03c6b79c61b6f6cca9715077f46d6
marcosencarnacion <enc24001@byui.edu>
2025-01-31 22:02:17 -0500

    attributes for the Address class added

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateOrProvince;
+    private string _country;
+
+    // Constructor to initialize the address
+}
