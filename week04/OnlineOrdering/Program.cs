577fa23e9076ad6201e6fc5731d30007b63303a9
marcosencarnacion <enc24001@byui.edu>
2025-01-31 22:27:02 -0500

    sample addresses are created

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Sample addresses created
+        Address address1 = new Address("123 Main St", "Los Angeles", "CA", "USA");
+        Address address2 = new Address("456 Maple Ave", "Toronto", "ON", "Canada");
+        Address address3 = new Address("1 Apple Park Way", "Cupertino", "CA", "USA");
+
+        //
     }
 }
