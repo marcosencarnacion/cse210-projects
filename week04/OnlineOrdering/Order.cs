db0966b8141b0c2316f1e9fb3c5b53c4ecdd45ec
marcosencarnacion <enc24001@byui.edu>
2025-01-31 20:56:15 -0500

    Constructor added.

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -5,4 +5,12 @@ public class Order
 {
     private List<Product> _products;
     private Customer _customer;
+
+    // Constructor 
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>(); // Initialize empty product list.
+    }
+
 }
