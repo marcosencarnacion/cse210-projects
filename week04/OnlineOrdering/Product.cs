fe039d0952165f0b852b4ab6de9ceefcdd118a55
marcosencarnacion <enc24001@byui.edu>
2025-01-31 21:42:46 -0500

    added attributes of the Product class

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+}
