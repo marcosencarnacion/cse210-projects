a5844b44ffc0ee21fd63b90de7b1d3087dec94cb
marcosencarnacion <enc24001@byui.edu>
2025-01-31 22:37:49 -0500

    3 more instances of products created

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -18,5 +18,8 @@ class Program
         Product product1 = new Product("Laptop", "LAP123456", 999.99, 2);
         Product product2 = new Product("Smartphone", "PHN654321", 499.99, 1);
         Product product3 = new Product("Headphones", "HDP789012", 109.99, 3);
+        Product product4 = new Product("Tablet", "TAB234567", 299.99, 4);
+        Product product5 = new Product("Smartwatch", "SWT345678", 199.99, 5);
+        Product product6 = new Product("Bluetooth Speaker", "BSP456789", 149.99, 6);
     }
 }
