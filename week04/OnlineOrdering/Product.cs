12985230a6b4270f80b86e6cc9a519a9f501311e
marcosencarnacion <enc24001@byui.edu>
2025-01-31 21:46:42 -0500

    Added constructor to initialize the product

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -4,7 +4,17 @@ using System.Collections.Generic;
 public class Product
 {
     private string _name;
-    private string _productId;
+    private int _productId;
     private double _price;
     private int _quantity;
+
+    // Constructor to initialize the product
+    public Product(string name, int productId, double price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+    
 }
