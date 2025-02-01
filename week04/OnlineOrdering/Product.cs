feebcea94a0ee73bc55487d839c94a0f0af0d57f
marcosencarnacion <enc24001@byui.edu>
2025-01-31 22:32:56 -0500

    modified datatype for prodcut Id

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -4,12 +4,12 @@ using System.Collections.Generic;
 public class Product
 {
     private string _name;
-    private int _productId;
+    private string _productId;
     private double _price;
     private int _quantity;
 
     // Constructor to initialize the product
-    public Product(string name, int productId, double price, int quantity)
+    public Product(string name, string productId, double price, int quantity)
     {
         _name = name;
         _productId = productId;
@@ -19,7 +19,7 @@ public class Product
 
     // Public properties (getters only, no setters to protect encasuplation)
     public string Name => _name;
-    public int ProductId => _productId;
+    public string ProductId => _productId;
     public double Price => _price;
     public int Quantity => _quantity;
 
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -9,9 +9,13 @@ class Program
         Address address2 = new Address("456 Maple Ave", "Toronto", "ON", "Canada");
         Address address3 = new Address("1 Apple Park Way", "Cupertino", "CA", "USA");
 
-        // Customers created
+        // Sample customers created
         Customer customer1 = new Customer("Emily Johnson", address1);
         Customer customer2 = new Customer("Michael Smith", address2);
         Customer customer3 = new Customer("Olivia Martinez", address3);
+
+        // Sample products created
+        Product product1 = new Product("Laptop", 101, 999.99, 2);
+        Product product2 = new Product("Smartphone", 102, 499.99, 1)
     }
 }
