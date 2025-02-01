ba7fb3a442ab0011c0ff8ef7650f755a44b208a3
marcosencarnacion <enc24001@byui.edu>
2025-01-31 22:04:02 -0500

    constructor is added to initialize the address

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -9,4 +9,11 @@ public class Address
     private string _country;
 
     // Constructor to initialize the address
+    public Address(string streetAddress, string city, string stateOrProvince, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
+    }
 }
