be2d0a13bf3a3c2a8a19ce070899e6ea2c914e42
marcosencarnacion <enc24001@byui.edu>
2025-01-31 21:59:04 -0500

    method to check if the customer lives in the USA added

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -20,6 +20,9 @@ public class Customer
     // Public property for accessing the customer's address (read-only)
     public Address Address => _address;
 
-    
-
+    // Method to check if the customer lives in the USA
+    public bool IsInUSA()
+    {
+        return _address.IsInUSA(); // Calls the IsInUSA method of the Address class
+    }
 }
