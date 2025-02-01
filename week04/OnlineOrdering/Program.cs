c2bdad6e799834a4516360097dd040700530a6c4
marcosencarnacion <enc24001@byui.edu>
2025-01-31 22:29:40 -0500

    instances of customers created.

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -9,6 +9,9 @@ class Program
         Address address2 = new Address("456 Maple Ave", "Toronto", "ON", "Canada");
         Address address3 = new Address("1 Apple Park Way", "Cupertino", "CA", "USA");
 
-        //
+        // Customers created
+        Customer customer1 = new Customer("Emily Johnson", address1);
+        Customer customer2 = new Customer("Michael Smith", address2);
+        Customer customer3 = new Customer("Olivia Martinez", address3);
     }
 }
