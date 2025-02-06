5d05b4367279a0d7bd33f75dfbd0d2714f276756
marcosencarnacion <enc24001@byui.edu>
2025-02-06 00:12:30 -0500

    updated the main program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -8,10 +8,12 @@ class Program
         Console.WriteLine(assignment.GetSummary());
 
         Assignment assignment1 = new Assignment("Roberto Rodriguez", "Fractions");
-        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
 
         WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment.GetSummary());
         Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
