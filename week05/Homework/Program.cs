3ce5f670017cb4d952f4e797011c584a0ea31237
marcosencarnacion <enc24001@byui.edu>
2025-02-06 00:10:02 -0500

    uncommented code

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Assignment assignment = new Assignment("Dylan Encarnacion", "Mathematics");
-        // Console.WriteLine(assignment.GetSummary());
+        Assignment assignment = new Assignment("Dylan Encarnacion", "Mathematics");
+        Console.WriteLine(assignment.GetSummary());
 
-        // Assignment assignment1 = new Assignment("Roberto Rodriguez", "Fractions");
-        // MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
-        // Console.WriteLine(mathAssignment.GetHomeworkList());
+        Assignment assignment1 = new Assignment("Roberto Rodriguez", "Fractions");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
 
         WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
         Console.WriteLine(writingAssignment.GetWritingInformation());
