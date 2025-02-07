4ba5c8b173ea54d638874c69b6e37b5f3e627448
marcosencarnacion <enc24001@byui.edu>
2025-02-07 14:24:54 -0500

    constructor added and variables initialized

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -5,4 +5,11 @@ public class Activity
     private string _name;
     private string _description;
     private int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
 }
