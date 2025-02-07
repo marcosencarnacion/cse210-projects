f6d15a68de3aec6b34af38928616d3f6bfe7f9b7
marcosencarnacion <enc24001@byui.edu>
2025-02-07 17:44:20 -0500

    updated "Console.Write"

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -6,6 +6,36 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
 
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(5);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            string s = animationStrings[i];
+            Console.Write(s);
+            Thread.Sleep(200);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i > animationStrings.Count)
+            {
+                i = 0;
+            }
+        }
+
     }
 
 }
