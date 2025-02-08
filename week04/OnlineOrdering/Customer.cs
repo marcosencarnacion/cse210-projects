9d4124dc3398328eb508f26a02425853593015a0
marcosencarnacion <enc24001@byui.edu>
2025-02-08 15:11:13 -0500

    Customer class created. Added its member variables and constructor.

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Net.Sockets;
+
+public class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+}
