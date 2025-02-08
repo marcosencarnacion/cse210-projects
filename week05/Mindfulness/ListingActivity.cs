bf151306bbe3b95343b96d6bf566424c10684397
marcosencarnacion <enc24001@byui.edu>
2025-02-07 23:32:42 -0500

    added a question mark to one of the questions for the listing activity

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -12,7 +12,7 @@ public class ListingActivity : Activity
         _prompts = new List<string>
         {
             "Who are people that you appreciate?",
-            "What are personal strengths of yours",
+            "What are personal strengths of yours?",
             "Who are people that you have helped this week?",
             "When have you felt the Holy Ghost this month?",
             "Who are some of your personal heroes?"
@@ -59,6 +59,7 @@ public class ListingActivity : Activity
         }
 
         Console.WriteLine($"You listed {_count} items!");
+        Console.WriteLine();
         DisplayEndingMessage($"You have completed another {sessionDuration} seconds of the Listing Activity.");
         ShowSpinner(5);
 
