b808720843e11b1ac24122dd9b7f65a22e8ad404
marcosencarnacion <enc24001@byui.edu>
2025-02-08 16:17:46 -0500

    Added functionality logic in the CaculateTotalCost method inside the Order class.

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -2,17 +2,36 @@ using System;
 
 public class Order
 {
-    private List<string> _products;
-    private string _customer;
+    private List<Product> _products;
+    private Customer _customer;
 
-    public Order(string customer)
+    public Order(Customer customer)
     {
-        _products = new List<string>();
+        _products = new List<Product>();
         _customer = customer;
     }
 
     public double CalculateTotalCost()
     {
+        double shippingCost;
+
+        if (_customer.IsInUSA())
+        {
+            shippingCost = 5;
+        }
+        else
+        {
+            shippingCost = 35;
+        }
+        double totalCost = 0;
+
+        foreach (var product in _products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+
+        totalCost += shippingCost;
+        return totalCost;
 
     }
 
