dba1db98c67a3bce6e021d3a1c21d865c72958b4
marcosencarnacion <enc24001@byui.edu>
2025-02-08 14:52:44 -0500

    Constructor added to initialize variables.

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -4,4 +4,15 @@ public class Order
 {
     private List<string> _products;
     private string _customer;
+
+    public Order(string customer)
+    {
+        _products = new List<string>();
+        _customer = customer;
+    }
+
+    public void CalculateTotalCost()
+    {
+
+    }
 }
