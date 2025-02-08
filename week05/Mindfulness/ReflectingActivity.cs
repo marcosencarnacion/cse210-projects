1b7d55c76b589940e9f3336f8cf58a501d8fa23f
marcosencarnacion <enc24001@byui.edu>
2025-02-07 23:50:53 -0500

    constructor added. Run() method added and called the DisplayStartingMessage. User input parsed to "int"

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -4,4 +4,22 @@ public class ReflectingActivity : Activity
 {
     private List<string> _prompts;
     private List<string> _questions;
+
+    public ReflectingActivity() : base("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strenght and resilience. This will help you recognize the power you have and how you an use it in other aspects of your life.", 0)
+    {
+
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage("Welcome to the Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strenght and resilience. This will help you recognize the power you have and how you an use it in other aspects of your life");
+
+        int sessionDuration = int.Parse(Console.ReadLine());
+        _duration = sessionDuration;
+
+        Console.Clear();
+
+    }
+
+
 }
