76ff02b01f7d95838a0f3e3a6a3cee2ce3ccaf33
marcosencarnacion <enc24001@byui.edu>
2025-02-07 19:50:41 -0500

    initiated Run method to apply inherited methods such as DisplayStartingMessage. Added int.Parse to convert user input to "int"

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -6,5 +6,14 @@ public class BreathingActivity : Activity
     {
     }
 
-    
+    public void Run()
+    {
+        DisplayStartingMessage("Welcome to the Breathing Activity.", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+
+        int sessionDuration = int.Parse(Console.ReadLine());
+
+        _duration = sessionDuration;
+    }
+
+
 }
