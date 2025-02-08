ba7e931bd97bd2223331709face74840a221cc2a
marcosencarnacion <enc24001@byui.edu>
2025-02-07 19:46:10 -0500

    BreathingActivity class added, and inheritance applied from Activity class.

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.", 0)
+    {
+    }
+
+    
+}
