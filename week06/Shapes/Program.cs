2b8df8d64698df7730bcde8a05024bfdc43227a5
marcosencarnacion <enc24001@byui.edu>
2025-02-14 16:47:03 -0500

    created Rectangle class. Added its member variables, constructor that gets the color, length and width and a GetArea method that calculates the area.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,10 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Square square = new Square("Blue", 3);
+        // Square square = new Square("Blue", 3);
 
-        Console.WriteLine($"Color of Square: {square.GetColor()}");
-        Console.WriteLine($"Area of Square: {square.GetArea()}");
+        // Console.WriteLine($"Color of Square: {square.GetColor()}");
+        // Console.WriteLine($"Area of Square: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Orange", 5, 7);
+        Console.WriteLine($"Color of Rectangle: {rectangle.GetColor()}");
+        Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+
     }
 
 }
