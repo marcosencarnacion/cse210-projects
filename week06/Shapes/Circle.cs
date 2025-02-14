6f5374d6d783bf7101c2553666f320b22773484c
marcosencarnacion <enc24001@byui.edu>
2025-02-14 17:00:38 -0500

    Created Circle class. Added its member variables, constructor, and GetArea() method. Instanciated to test.

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.Round(Math.PI * Math.Pow(_radius, 2), 2);
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,14 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Square square = new Square("Blue", 3);
-
-        // Console.WriteLine($"Color of Square: {square.GetColor()}");
-        // Console.WriteLine($"Area of Square: {square.GetArea()}");
+        Square square = new Square("Blue", 3);
+        Console.WriteLine($"Color of Square: {square.GetColor()}");
+        Console.WriteLine($"Area of Square: {square.GetArea()}");
+        Console.WriteLine();
 
         Rectangle rectangle = new Rectangle("Orange", 5, 7);
         Console.WriteLine($"Color of Rectangle: {rectangle.GetColor()}");
         Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+        Console.WriteLine();
+
+        Circle circle = new Circle("Green", 5);
+        Console.WriteLine($"Color of Circle: {circle.GetColor()}");
+        Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+        Console.WriteLine();
+
 
     }
 
