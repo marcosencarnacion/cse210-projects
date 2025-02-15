1f80d752f93925bbda152ca84f3dd5264839e47b
marcosencarnacion <enc24001@byui.edu>
2025-02-15 12:17:51 -0500

    corrected spelling and a typo

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -5,11 +5,11 @@
     1- User Input Handling:
         -Added input validation throughout the program, for the main menu, submenus, etc.
         -This ensures that the user only selects valid entries and correct inputs (e.g. positive numbers, options from the menu, etc.)
-        -This helps to enahance the user experience and avoid errors.
+        -This helps to enhance the user experience and avoid errors.
 
     2- Delete Goal Functionality:
-        -I added a DeleteGoal() method inside the GoalManager class. This allows the user remove any goals they no longer want to set or track.
-        -This helps the user decide if a goal is no longer needed, allowing the program to be more flexibile and user-friendly. 
+        -I added a DeleteGoal() method inside the GoalManager class. This allows the user to remove any goals they no longer want to set or track.
+        -This helps the user decide if a goal is no longer needed, allowing the program to be more flexible and user-friendly. 
 
 */
 
