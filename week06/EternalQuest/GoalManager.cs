ae2598c02c7816487331f971e969bff84ed5faf2
marcosencarnacion <enc24001@byui.edu>
2025-02-14 22:43:01 -0500

    added a menu, and a while loop to loop through the menu.

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -14,15 +14,51 @@ public class GoalManager
 
     public void Start()
     {
-        Console.WriteLine($"You have {_score} points.");
-        Console.WriteLine("\nMenu Options:");
-        Console.WriteLine("  1. Create New Goal");
-        Console.WriteLine("  2. List Goals");
-        Console.WriteLine("  3. Save Goals");
-        Console.WriteLine("  4. Load Goals");
-        Console.WriteLine("  5. Record Event");
-        Console.WriteLine("  6. Quit");
-        Console.WriteLine("Select a choice from the menu: ");
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine($"You have {_score} points.");
+            Console.WriteLine("\nMenu Options:");
+            Console.WriteLine("  1. Create New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case "1":
+                    CreateGoal();
+                    break;
+                case "2":
+                    ListGoalDetails();
+                    break;
+                case "3":
+                    SaveGoals();
+                    break;
+                case "4":
+                    LoadGoals();
+                    break;
+                case "5":
+                    RecordEvent();
+                    break;
+                case "6":
+                    running = false;
+                    Console.WriteLine("You have finished managing your goals today.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry. Please enter a number between 1 and 6.");
+                    break;
+
+            }
+        }
+
+
     }
     public void DisplayPlayerInfo()
     {
