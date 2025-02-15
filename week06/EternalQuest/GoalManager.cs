2186a49205d18ead2ef4e183a72423b4f5287b79
marcosencarnacion <enc24001@byui.edu>
2025-02-14 22:17:52 -0500

    added menu inside the Start() method.

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -14,7 +14,15 @@ public class GoalManager
 
     public void Start()
     {
-
+        Console.WriteLine($"You have {_score} points.");
+        Console.WriteLine("\nMenu Options:");
+        Console.WriteLine("  1. Create New Goal");
+        Console.WriteLine("  2. List Goals");
+        Console.WriteLine("  3. Save Goals");
+        Console.WriteLine("  4. Load Goals");
+        Console.WriteLine("  5. Record Event");
+        Console.WriteLine("  6. Quit");
+        Console.WriteLine("Select a choice from the menu: ");
     }
     public void DisplayPlayerInfo()
     {
