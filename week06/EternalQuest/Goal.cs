d2d03764ad5939a518cb4ddb0d1f1ba126a46b98
marcosencarnacion <enc24001@byui.edu>
2025-02-14 20:35:32 -0500

    Member variables added in the Goal class, constructor and methods. Methods are empty for now.

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,25 @@
+using System;
+
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string shortName, string description, int points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+    public string GetDetailsString()
+    {
+        return "";
+    }
+    public abstract string GetStringRepresentation();
+
+}
