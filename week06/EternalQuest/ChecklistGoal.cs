eb0b1af951f4b7a1d5271b32393a63ce7534c66c
marcosencarnacion <enc24001@byui.edu>
2025-02-14 21:31:55 -0500

    variables, constructor and methods added to the ChecklistGoal class.

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string shortName, string description, int points, int target, int bonus) : base(shortName, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+
+    public override void RecordEvent()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool IsComplete()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetDetailsString()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetStringRepresentation()
+    {
+        throw new NotImplementedException();
+    }
+}
