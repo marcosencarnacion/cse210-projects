4ff258ae5a87253ad4ae72864acf0609bdd38f27
marcosencarnacion <enc24001@byui.edu>
2025-02-17 22:41:43 -0500

    Activity class created. Its member variables and methods.

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,33 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _duration;
+
+    public Activity(DateTime date, int duration)
+    {
+        _date = date;
+        _duration = duration;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{_date:dd MMMM yyyy} {this.GetType().Name} ({_duration} min) - " +
+        $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
+    }
+}
